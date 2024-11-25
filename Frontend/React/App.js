ec01fb1a12ae83cebafb3748a846214ba0c439de
Hari851 <66940854+Hari851@users.noreply.github.com>
2024-11-24 20:37:34 -0600

    Create App.js

---
+++ Frontend/React/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StockComponent from "./StockComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <StockComponent />
+    </div>
+  );
+}
+
+export default App;
